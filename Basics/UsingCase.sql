1d0497125128358e8d84ffa58bc3f866442890b6
Kavi bharathi Chinnathambi <57504930+ckbharathi92@users.noreply.github.com>
2020-06-10 16:04:54 -0500

    Different Variants of  Case

--- Basics/UsingCase.sql
+++ Basics/UsingCase.sql
@@ -6,3 +6,19 @@ select (case when ((A+B) <= C) or ((A+C) <= B) or ((C+B) <= A) then 'Not A Trian
              when (A != B) or (B !=C) or (A !=C ) then 'Scalene'
         end ) 
 from triangles  
+
+# Case with CONCAT
+
+select 
+        case 
+			when occupation = 'Actor' then concat('There are a total of ', count, ' ',lower(occupation), 's')
+			when occupation = 'Doctor' then concat('There are a total of ', count, ' ',lower(occupation), 's')
+			when occupation = 'Singer' then concat('There are a total of ', count, ' ',lower(occupation), 's')
+			when occupation = 'Professor' then concat('There are a total of ', count, ' ',lower(occupation), 's')
+        end
+from (	
+        select  occupation, 
+                count(occupation) as count 
+        from occupations 
+        group by occupation ) a
+order by count, occupation;
